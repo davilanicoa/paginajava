2a7a5109947aa4562175ea7c42496ca421134132
Nicolas Davila <nicolasdavila12@gmail.com>
2021-07-26 23:56:40 -0300

    gittesigoodiando

---
+++ index2.js
@@ -0,0 +1,33 @@
+// PEDIR 4 DATOS
+// NOMBRE - CONTRASEÑA - AÑO DE NACIMIENTO - AÑO ACTUAL
+// INGRESAR 
+
+// SI LA CONTRASEÑA ES "coderhouse"
+// TENGO QUE MOSTRAR ALERT mi edad actual
+// "JULIAN TENES 30 AÑOS"
+
+//  SINO digo que la contraseña es incorrecta
+
+/*let nombre = prompt("INGRESA TU NOMBRE")
+let nacimiento = Number(prompt("INGRESA FECHA DE NACIMIENTO"))
+let anio = Number(prompt("INGRESE AÑO ACTUAL"))
+let contraseña = prompt("INGRESE SU CONTRASEÑA")
+
+let edad = anio - nacimiento
+
+if (contrasenia == "coderhouse"){
+    alert(nombre + "tenes" + edad + "años")
+
+}else{
+    alert("CONTRASEÑA INCORRECTA")
+
+}
+*/
+
+let unNumero = Number(prompt("UN NUMERO"))
+let unNumeroNuevo = Number(prompt("UN NUMERO DOS"))
+
+if (unNumero > 1000){
+    alert("EL NUMERO ES MAYOR A 1000");
+}
+
